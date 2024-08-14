cec706560e3c2d071dc5fa9984ceec4b1d00788e
siwoong28 <165887841+siwoong28@users.noreply.github.com>
2024-08-14 21:12:36 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int price) {
+        double answer = 0;
+        if(price>=500000){
+            answer = price*0.8;
+        }
+        else if(price<500000&&price>=300000){
+            answer = price*0.9;
+        }
+        else if(price>=100000){
+            answer =  price*0.95;
+        }
+        else{
+            answer = price;
+        }
+        return (int)answer;
+    }
+}
