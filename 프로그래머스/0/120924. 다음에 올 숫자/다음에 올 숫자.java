ac61080e59c530e0b46206401173692608f57500
siwoong28 <165887841+siwoong28@users.noreply.github.com>
2024-09-15 19:37:45 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+            if(common[1]-common[0]==common[2]-common[1]){
+                answer = common[common.length-1]+common[1]-common[0];
+            }
+            else if(common[0]!=0&&common[1]!=0){
+                if (common[1] / common[0] == common[2] / common[1]) {
+                answer = common[common.length - 1] * (common[1] / common[0]);
+            }
+        }
+        else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
