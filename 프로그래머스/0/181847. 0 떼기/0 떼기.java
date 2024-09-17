c8e9a7c9fc775c973dd364ef1a4264e0daaab6fc
siwoong28 <165887841+siwoong28@users.noreply.github.com>
2024-09-17 21:44:55 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int temp = Integer.parseInt(n_str);
+        answer =  Integer.toString(temp); 
+        return answer;
+    }
+}
